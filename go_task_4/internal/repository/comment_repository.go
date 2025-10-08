196345294626a0abbb0b86d6127d4ffcc4231b7c
者者22 <65362915@qq.com>
2025-10-08 15:42:27 +0800

    222

--- go_task_4/internal/repository/comment_repository.go
+++ go_task_4/internal/repository/comment_repository.go
@@ -1,1 +1,66 @@
 package repository
+
+import (
+	"go_task_4/internal/model"
+
+	"gorm.io/gorm"
+)
+
+type CommentRepositoryInterface interface {
+	Create(comment *model.Comment) error
+	GetByID(id int) (*model.Comment, error)
+	GetByUserID(userid int) ([]*model.Comment, error)
+	GetByPostID(postid int) ([]*model.Comment, error)
+	Update(comment *model.Comment) error
+	Delete(id int) error
+	Count() (int, error)
+}
+
+// 注入db实例
+type CommentRepository struct {
+	db *gorm.DB
+}
+
+func NewCommentRepository(db *gorm.DB) *CommentRepository {
+	return &CommentRepository{db: db}
+}
+
+func (c *CommentRepository) Create(comment *model.Comment) error {
+	err := c.db.Model(&model.Comment{}).Create(comment).Error
+	return err
+}
+
+func (c *CommentRepository) GetByID(id int) (*model.Comment, error) {
+	comment := model.Comment{}
+	err := c.db.Model(&model.Comment{}).Where("id = ?", id).First(&comment).Error
+	return &comment, err
+}
+
+func (c *CommentRepository) GetByUserID(id int) (*[]model.Comment, error) {
+	comments := []model.Comment{}
+	err := c.db.Model(&model.Comment{}).Where("user_id = ?", id).Find(&comments).Error
+	return &comments, err
+}
+
+func (c *CommentRepository) GetByPostID(id int) (*[]model.Comment, error) {
+	comments := []model.Comment{}
+	err := c.db.Model(&model.Comment{}).Where("post_id = ?", id).Find(&comments).Error
+	return &comments, err
+}
+
+func (c *CommentRepository) Update(comment model.Comment) error {
+	err := c.db.Model(&model.Comment{}).Save(&comment).Error
+	return err
+}
+
+// 软删除
+func (c *CommentRepository) Delete(comment model.Comment) error {
+	err := c.db.Model(&model.User{}).Delete(&comment).Error
+	return err
+}
+
+func (c *CommentRepository) Count() error {
+	var total int64
+	err := c.db.Model(&model.Comment{}).Count(&total).Error
+	return err
+}
--- go_task_4/internal/repository/post_repository.go
+++ go_task_4/internal/repository/post_repository.go
@@ -1,1 +1,67 @@
 package repository
+
+import (
+	"go_task_4/internal/model"
+
+	"gorm.io/gorm"
+)
+
+type PostRepositoryInterface interface {
+	Create(post *model.Post) error
+	GetByID(id int) (*model.Post, error)
+	GetByUserID(userid int) ([]*model.Post, error)
+	FindAll() ([]*model.Post, error)
+	Update(post *model.Post) error
+	Delete(id int) error
+	Count() (int, error)
+}
+
+// 注入db实例
+type PostRepository struct {
+	db *gorm.DB
+}
+
+func NewPostRepository(db *gorm.DB) *PostRepository {
+	return &PostRepository{db: db}
+}
+
+func (p *PostRepository) Create(post *model.Post) error {
+	err := p.db.Model(&model.Post{}).Create(post).Error
+	return err
+}
+
+func (p *PostRepository) GetByID(id int) (*model.Post, error) {
+	post := model.Post{}
+	err := p.db.Model(&model.Post{}).Where("id = ?", id).First(&post).Error
+	return &post, err
+}
+
+func (p *PostRepository) GetByUserID(id int) (*[]model.Post, error) {
+	posts := []model.Post{}
+	err := p.db.Model(&model.Post{}).Where("user_id = ?", id).Find(&posts).Error
+	return &posts, err
+}
+
+func (p *PostRepository) FindAll() ([]model.Post, error) {
+	var posts []model.Post
+	// Find all posts and preload the user for each post
+	err := p.db.Preload("User").Order("created_at desc").Find(&posts).Error
+	return posts, err
+}
+
+func (p *PostRepository) Update(post model.Post) error {
+	err := p.db.Model(&model.Post{}).Save(&post).Error
+	return err
+}
+
+// 软删除
+func (p *PostRepository) Delete(post model.Post) error {
+	err := p.db.Model(&model.User{}).Delete(&post).Error
+	return err
+}
+
+func (p *PostRepository) Count() error {
+	var total int64
+	err := p.db.Model(&model.Post{}).Count(&total).Error
+	return err
+}
